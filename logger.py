7e08921e64d3902942d3b4d501f5c31b5499e9bc
NoraxMine <NoraxMine@tutamail.com>
2025-04-03 19:41:33 +0300

    lz3f

--- logger.py
+++ logger.py
@@ -9,28 +9,32 @@ def date():
     date = datetime.datetime.now().strftime('%Y-%m-%d')         #получаем дату
     return date
 def time():
-    date_and_time = str(datetime.datetime.now()).split()            #отделяем время от даты
-    time = date_and_time[1]         #берём только время
+    time =datetime.datetime.now().strftime('%H:%M:%S')
     return time
+    
 
 def logger(func):
     def wrapper(*args, **kwargs):
         res  = func(*args, **kwargs)
         f_name = func.__name__
 
-        if os.path.isfile('name.csv'):
-            csv = pd.read_csv('name.csv')
+        log_file = 'logs.csv'
+
+        if os.path.isfile('logs.csv'):
+            csv = pd.read_csv('logs.csv')
             columns = {'': [len(csv)], "Пользователь": [user], "Действие": [f_name],"Дата": [date()],"Время":[time()]}
             DF = pd.DataFrame(columns)
-            DF.to_csv('name.csv', mode = 'r', index = False, header = False)
+            DF.to_csv('logs.csv', mode = 'a', index = False, header = False)
         else:
             columns = {"Пользователь": [user], "Действие": [f_name],"Дата": [date()],"Время":[time()]}
             DF = pd.DataFrame(columns)
-            DF.to_csv('name.csv')
+            DF.to_csv('logs.csv')
         return res
     return wrapper
     
 @logger
 def yupi():
     print("yuppi")
-yupi()
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from diag import Diagrama
+from logger import yupi
 
 if __name__ == "__main__":
     crug = Diagrama("personal_transactions.csv")
     crug.diagrama()
-
+    yupi()
